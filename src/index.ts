831c70e41deac752535195c622b0aaad7cd2beec
idsbllp <idsbllp@gmail.com>
2020-08-07 23:11:58 +0800

    feat: init promise constructor
    Change-Id: I54d95ef080666c1d1c53a662a70c052b4e32771a

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import PromiseA from './promise';
+
+const promiseA1 = new PromiseA<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve('string');
+  }, 1000);
+});
+
+// console.log('promiseA1 111: ', promiseA1);
+
+
+const promise2 = promiseA1.then(data => {
+// const promiseA2 = promiseA1.then(data => {
+  console.log(111, data);
+
+  return 22222;
+}).then(res => {
+  console.log(222222222222, res, promise2.status);
+  return {};
+});
+
+promise2.then(res => {
+  console.log('promise2 res: ', res, promise2.status);
+});
+
+promiseA1.then(res => {
+  console.log(33333333333, res);
+});
+
+// console.log('promiseA1 222: ', promiseA1);
+
+// console.log('promiseA2 333: ', promiseA2);
+
+// const promise3 = promiseA2.then(data => {
+//   console.log(222, data);
+
+//   return {};
+// });
+
+// console.log('promiseA1 === promiseA2: ', promiseA1 === promiseA2, promiseA2 === promise3);
+
+
+// for compare
+// const promise = new Promise<number>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(12345);
+//   }, 1000);
+// });
+
+// promise.then(data => {
+//   console.log(111, data);
+
+//   return 'string';
+// }).then(data => {
+//   console.log(222, data);
+// });
---
+++ src/promise.ts
@@ -0,0 +1,91 @@
+enum Status {
+  Pending = 'pending',
+  Fulfilled = 'fulfilled',
+  Rejected = 'rejected',
+}
+
+type TFulfilledFn<T, TResult = void> = (value: T) => TResult;
+type TRejectedFn = (error: Error) => void;
+
+type TExecutorFn<T> = (resolve: TFulfilledFn<T>, reject?: TRejectedFn) => void;
+
+interface IDeferred<TResult, T> {
+  promise: PromiseA<TResult>;
+    onFulfilled: TFulfilledFn<T, TResult>;
+    onRejected: TRejectedFn | undefined;
+}
+
+function noop() {
+  //
+}
+
+function doResolveAsync<T>(executor: TExecutorFn<T>, promiseA: PromiseA<T>) {
+  function _resolve(value: T) {
+    handleResolve(promiseA, value);
+  }
+
+  executor(_resolve);
+}
+
+function handleResolve<T, TValue>(promiseA: PromiseA<T>, value: TValue) {
+  promiseA.value = value;
+  promiseA.status = Status.Fulfilled;
+
+  promiseA.deferredCallback.forEach(deferred => {
+    handleDeferred(promiseA, deferred);
+  });
+
+  promiseA.deferredCallback = [];
+}
+
+/**
+ * 处理 deferred
+ * 当状态为 Pending 时， 向 deferred 属性中追加
+ * 当状态为 Fulfilled 或者 Rejected 时，延时分别执行对应函数
+ */
+function handleDeferred<TResult, T>(promiseA: PromiseA<T>, deferred: IDeferred<TResult, T>) {
+  if (promiseA.status === Status.Pending) {
+    promiseA.deferredCallback.push(deferred);
+    return;
+  }
+
+  // 模拟延时
+  setImmediate(() => {
+    const callback = promiseA.status === Status.Fulfilled ? deferred.onFulfilled : deferred.onRejected;
+
+    if (!callback) return;
+
+    const value = callback(promiseA.value);
+
+    // 处理 then 返回的 promise
+    handleResolve(deferred.promise, value);
+  });
+}
+
+class PromiseA<T = any> {
+  value: any;
+
+  deferredCallback: IDeferred<any, T>[]  = [];
+
+   status = Status.Pending;
+
+  constructor(executor: TExecutorFn<T>) {
+    doResolveAsync(executor, this);
+  }
+
+  then<TResult>(onFulfilled: TFulfilledFn<T, TResult>, onRejected?: TRejectedFn) {
+    const promiseB = new PromiseA<TResult>(noop);
+
+    const deferred: IDeferred<TResult, T> = {
+      promise: promiseB,
+      onFulfilled,
+      onRejected,
+    };
+
+    handleDeferred(this, deferred);
+
+    return promiseB;
+  }
+}
+
+export default PromiseA;
